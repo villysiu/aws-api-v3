9a1bbbd266ec97ef3538304282f054c4b2d68b93
Villy Siu <villysiu@gmail.com>
2023-04-25 22:28:35 -0700

    Add basic aws eb deploy config

---
+++ .platform/hooks/*/*.sh
@@ -0,0 +1,8 @@
+chmod +x .platform/hooks/prebuild/01_config_database.sh
+git update-index --chmod=+x .platform/hooks/prebuild/01_config_database.sh
+
+chmod +x .platform/hooks/prebuild/02_install_node_and_yarn.sh
+git update-index --chmod=+x .platform/hooks/prebuild/02_install_node_and_yarn.sh
+
+chmod +x .platform/hooks/prebuild/03_install_packages.sh
+git update-index --chmod=+x .platform/hooks/prebuild/03_install_packages.sh
